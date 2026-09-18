b1ab85e3e6ecbc8b33fa07e19b55813c29571de7
eddaniel123 <danielmision2022@gmail.com>
2026-09-18 12:10:56 -0500

    Configura STATIC_ROOT para producción

--- sistema_obstetrico/settings.py
+++ sistema_obstetrico/settings.py
@@ -337,6 +337,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "trabajoparto/static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
